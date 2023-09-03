e5a13b031eaee65dc71b66d7de2949429a6af145
José Javier Blanco Rivero <javierweiss@gmail.com>
2023-09-02 21:44:58 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to javierweiss/luhmann-chat
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/javierweiss/luhmann_chat_test.clj
@@ -0,0 +1,7 @@
+(ns javierweiss.luhmann-chat-test
+  (:require [clojure.test :refer :all]
+            [javierweiss.luhmann-chat :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
