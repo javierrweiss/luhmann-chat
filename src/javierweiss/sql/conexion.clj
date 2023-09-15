5ba635db786376bedbfed30bdcaa6ddecf5641d9
José Javier Blanco Rivero <56279998+javierrweiss@users.noreply.github.com>
2023-09-15 14:10:58 +0000

    Completar conexión y crear extension vector

--- src/javierweiss/sql/conexion.clj
+++ src/javierweiss/sql/conexion.clj
@@ -10,15 +10,20 @@
   [sentence]
   (with-open [^HikariDataSource d (connection/->pool com.zaxxer.hikari.HikariDataSource
                                                      {:dbtype (:dbtype conf)
-                                                      :db-name (:dbname conf)
-                                                      :user (:user conf) 
+                                                      :db-name (:db-name conf)
+                                                      :username (:user conf) 
                                                       :password (:password conf)
                                                       :host (:host conf)})]
-    (jdbc/execute! d sentence)))
+    (jdbc/execute! d sentence))) 
 
+
+(defn activar-extension
+  []
+  (ejecuta-sentencia ["CREATE EXTENSION IF NOT EXISTS vector"]))
 
 (comment 
    
   (ejecuta-sentencia ["SELECT 1"])
-   
+   (activar-extension)
+  (ejecuta-sentencia ["SELECT * FROM pg_extension"])
   )
