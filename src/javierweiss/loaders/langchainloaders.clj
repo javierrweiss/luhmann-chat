c64f74393a6b5fdeeaf2e8ef392f5cd4a08f6399
José Javier Blanco Rivero <56279998+javierrweiss@users.noreply.github.com>
2023-11-16 02:07:10 +0000

    Loaders module

---
+++ src/javierweiss/loaders/langchainloaders.clj
@@ -0,0 +1,6 @@
+(ns javierweiss.loaders.langchainloaders
+  (:require [libpython-clj2.python :as py :refer [py. py.. py.-]]))
+
+(def azure-blob (py/from-import langchain.document_loaders AzureBlobStorageContainerLoader))
+
+(def aws-s3 (py/from-import langchain.document_loaders S3FileLoader))
