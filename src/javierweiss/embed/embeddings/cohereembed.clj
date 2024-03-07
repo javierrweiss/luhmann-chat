8eb2a673ca3c8b58033cb5e6d1e471c7a2a5e4b9
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-03-07 22:08:02 +0000

    Refactorizar interfaz de embeddings

--- src/javierweiss/embed/embeddings/cohereembed.clj
+++
@@ -1,9 +0,0 @@
-(ns javierweiss.embed.embeddings.cohereembed
-  (:require [cohere.client :refer [embed]]
-            [javierweiss.configuracion.config :refer [configuracion]]))
-
-(System/setProperty "cohere.api.key" (-> (configuracion) :llm-service :cohere :cohere-key))
-
-(def cohere-embedding "& {:keys [texts model truncate], :or {model \"embed-english-v2.0\", truncate \"END\"}}" embed)
-
-
