ce56cd2b33fc353534fb07e9039bc3f1c7ff30a4
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-05-11 17:22:16 +0000

    Formatear consulta con honeysql

--- src/javierweiss/backend/db/sql/consultas.clj
+++ src/javierweiss/backend/db/sql/consultas.clj
@@ -1,7 +1,10 @@
 (ns javierweiss.backend.db.sql.consultas
   (:require [javierweiss.backend.db.sql.conexion :refer [ejecuta-sentencia]]
+            [honey.sql :as sql]
             [com.brunobonacci.mulog :as u])
   (:import java.sql.SQLException))
+
+(map sql/register-op! [:<=> :<-> :<#>])
 
 (defn buscar_similitudes
   "Recibe datos de conexión `opts`, array de floats `embeddings_consulta` y una llave entre las siguientes (`algoritmo`):
@@ -10,14 +13,37 @@
    :inner-product"
   [opts embeddings_consulta algoritmo]
   (let [algo (case algoritmo
-               :cosine-distance "<=>"
-               :l2-distance "<->"
-               :inner-product "<#>")
+               :cosine-distance  :<=>
+               :l2-distance  :<->
+               :inner-product :<#>)
         emb (->> embeddings_consulta first (interpose ", ") (apply str))
-        consulta [(str "SELECT contenido, referencia  
-                       FROM archivo_luhmann
-                       ORDER BY embedding " algo " '[" emb "]' LIMIT 5")]]
+        consulta (sql/format {:select [:contenido :referencia]
+                              :from :archivo-luhmann
+                              :order-by [[[algo :embedding [:inline emb]]]]
+                              :limit 5})]
     (try
       (ejecuta-sentencia consulta opts)
       (catch SQLException e (u/log ::excepcion-busqueda-embeds :mensaje (.getMessage e))))))
 
+
+(comment
+  
+
+  (let [algoritmo :cosine-distance 
+        embeddings_consulta [(into [] (take 100 (repeatedly rand)))]
+        algo (case algoritmo
+               :cosine-distance  :<=>
+               :l2-distance  :<->
+               :inner-product :<#>)
+        emb (->> embeddings_consulta first (interpose ", ") (apply str))
+        consulta (sql/format {:select [:contenido :referencia]
+                              :from :archivo-luhmann
+                              :order-by [[[algo :embedding [:inline emb]]]]
+                              :limit 5})]
+    consulta)
+  
+  (sql/format {:select :*
+               :from :items
+               :order-by [[[:<=> :embedding [:inline (str (range 1 100))]]]]})
+ 
+ :rcf)
